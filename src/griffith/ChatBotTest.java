2ad2576067493b68bc84b86f3c0095b85cca2013
Dylan Donnell <dylan.donnell@student.griffith.ie>
2025-03-21 19:20:17 +0000

    feat: add test for getLocationData

---
+++ src/griffith/ChatBotTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+
+class ChatBotTest {
+    final String[] CITIES = {
+            "Cork", "London", "Las Vegas", "Fake City 10000"
+    };
+
+    @Test
+    void getLocationData() {
+        int i = 0;
+        while (i != CITIES.length) {
+            JSONObject json = ChatBot.getLocationData(CITIES[i++]);
+
+            // fake city should not return value
+            if (i == CITIES.length) {
+                assertNull(json);
+                break;
+            }
+
+            // currently used keys, can be added to if more of the json response is used
+            assertNotNull(json);
+            assertNotNull(json.get("latitude"));
+            assertNotNull(json.get("longitude"));
+        }
+    }
+
+}
